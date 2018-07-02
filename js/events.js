b8abeacc6952995907c427d2da887a2829f784ff
Danielle Deavens <danielledeavens@gmail.com>
2018-07-02 18:45:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,19 +6,19 @@ $(document).ready(function(){
 
 });
 
-$('p').on("click",function(getIt){"Hey!"});
+$('p').on("click",function(){"Hey!"});
 
-$('img').on('load', function(frameIt){
+$('img').on('load', function(){
   border: red;
 });
 
-$(document).on('keydown', function(pressIt) {
+$(document).on('keydown', function() {
   if(key.which == G){
       alert('s was pressed');
   }
 });
 
-$("form").on("submit", function(submitIt) {
+$("form").on("submit", function() {
   if ($( "input:first" ).val() === "correct") {
     alert('your form is going to be submitted now');
     return;
